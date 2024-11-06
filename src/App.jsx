621a4d8a1585b38c9011af603c3e2a376a704e1e
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-11-06 21:15:47 +0100

    deployemet1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-600">
+      <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
     </>
