950ad3e90df46c97d1f7330ec7587f5fb7b0d221
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-11-06 19:50:00 +0100

    deployement

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
+    <>
+      <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
-    </div>
-  )
+    </>
+  );
 }
 
 export default App
