f5b804b8742dfc1f507cfb2501c24891c49404c6
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-11-07 08:47:43 +0100

    3d box in the middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react'
+import { BoxGeometry } from 'three';
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Canvas
+        camera={{
+          position: [3, 3, 3],
+          fov: 45,
+        }}
+      >
+        <OrbitControls />
+        <mesh>
+          <boxGeometry args={[0.5, 0.5, 0.5]} />
+          <meshNormalMaterial />
+        </mesh>
+        <color attach="background" args={["#130f30"]} />
+      </Canvas>
     </>
   );
 }
