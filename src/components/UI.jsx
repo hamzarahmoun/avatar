976a77cc655371aa5bd01494cad1d17026e36553
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-11-21 10:11:03 +0100

    add removable bottom

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { pb, useConfiguratorStore } from "../store.js";
 import { Color } from "three";
 const DownloadButton = () => {
-   const download = useConfiguratorStore((state) => state.download); 
+    const download = useConfiguratorStore((state) => state.download);
     return (
         <button
             className="rounded-lg bg-indigo-500 hover:bg-indigo-600 transition-colors duration-300 text-white font-medium px-4 py-3 pointer-events-auto drop-shadow-md"
@@ -21,7 +21,7 @@ const AssetsBox = () => {
         fetchCategories,
         setCurrentCategory,
         changeAsset,
-        customization, 
+        customization,
         lockedGroups,
     } = useConfiguratorStore();
 
@@ -32,7 +32,7 @@ const AssetsBox = () => {
             <div className="flex items-center gap-8 pointer-events-auto overflow-x-auto px-6 pb-2 noscrollbar">
                 {categories?.map((category) => (
                     <button
-                        key={category.id} 
+                        key={category.id}
                         onClick={() => setCurrentCategory(category)}
                         className={`transition-colors duration-200 font-medium flex-shrink-0 border-b
                             ${currentCategory?.name === category.name
@@ -45,23 +45,53 @@ const AssetsBox = () => {
                 ))}
             </div>
             <div className="flex gap-2 flex-row">
-                {currentCategory?.assets?.map((asset) => (
+                {currentCategory?.removable && (
+                    <button
+                        onClick={() => changeAsset(currentCategory.name, null)}
+                        className={`w-20 h-20 flex-shrink-0 rounded-xl overflow-hidden pointer-events-auto hover:opacity-100 transition-all border-2 duration-300
+              bg-gradient-to-tr
+              ${!customization[currentCategory.name].asset
+                                ? "border-white from-white/20 to-white/30"
+                                : "from-black/70 to-black/20 border-black"
+                            }`}
+                    >
+                        <div className="w-full h-full flex items-center justify-center bg-black/40 text-white">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-8"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18 18 6M6 6l12 12"
+                                />
+                            </svg>
+
+                        </div>
+                    </button>
+                )}
+                {currentCategory?.assets.map((asset) => (
                     <button
                         key={asset.thumbnail}
                         onClick={() => changeAsset(currentCategory.name, asset)}
-                        className={`w-20 h-20 rounded-md overflow-hidden bg-gray-200 pointer-events-auto hover:opacity-100 transition-all border-2 duration-500
-                            ${customization[currentCategory.name]?.id === asset.id
-                                ? "border-indigo-600 opacity-100"
-                                : "opacity-50 border-gray-300"
-                            }
-                        `}
+                        className={`w-20 h-20  flex-shrink-0 rounded-xl overflow-hidden pointer-events-auto hover:opacity-100 transition-all border-2 duration-300
+              bg-gradient-to-tr
+              ${customization[currentCategory.name]?.asset?.id === asset.id
+                                ? "border-white from-white/20 to-white/30"
+                                : "from-black/70 to-black/20 border-black"
+                            }`}
                     >
                         <img
-                        className="object-cover w-full h-full"
+                            className="object-cover w-full h-full"
                             src={pb.files.getUrl(asset, asset.thumbnail)}
                         />
                     </button>
                 ))}
+                
             </div>
         </div>
     );
@@ -84,7 +114,7 @@ export const UI = () => {
                 </div>
                 <div className="px-10 flex flex-col">
                     {currentCategory?.colorPalette &&
-                    customization[currentCategory.name] && <ColorPicker/>
+                        customization[currentCategory.name] && <ColorPicker />
                     }
                     <AssetsBox />
                 </div>
@@ -96,36 +126,35 @@ export const UI = () => {
 const ColorPicker = () => {
     const updateColor = useConfiguratorStore((state) => state.updateColor);
     const currentCategory = useConfiguratorStore(
-      (state) => state.currentCategory
+        (state) => state.currentCategory
     );
     const handleColorChange = (color) => {
-      updateColor(color);
+        updateColor(color);
     };
     const customization = useConfiguratorStore((state) => state.customization);
-  
+
     if (!customization[currentCategory.name]?.asset) {
-      return null;
+        return null;
     }
     return (
-      <div className="pointer-events-auto relative flex gap-2 max-w-full overflow-x-auto backdrop-blur-sm py-2 drop-shadow-md noscrollbar px-2 md:px-0">
-        {currentCategory.expand?.colorPalette?.colors.map((color, index) => (
-          <button
-            key={`${index}-${color}`}
-            className={`w-10 h-10 p-1.5 drop-shadow-md bg-black/20 shrink-0 rounded-lg overflow-hidden transition-all duration-300 border-2
-               ${
-                 customization[currentCategory.name].color === color
-                   ? "border-white"
-                   : "border-transparent"
-               }
+        <div className="pointer-events-auto relative flex gap-2 max-w-full overflow-x-auto backdrop-blur-sm py-2 drop-shadow-md noscrollbar px-2 md:px-0">
+            {currentCategory.expand?.colorPalette?.colors.map((color, index) => (
+                <button
+                    key={`${index}-${color}`}
+                    className={`w-10 h-10 p-1.5 drop-shadow-md bg-black/20 shrink-0 rounded-lg overflow-hidden transition-all duration-300 border-2
+               ${customization[currentCategory.name].color === color
+                            ? "border-white"
+                            : "border-transparent"
+                        }
             `}
-            onClick={() => handleColorChange(color)}
-          >
-            <div
-              className="w-full h-full rounded-md"
-              style={{ backgroundColor: color }}
-            />
-          </button>
-        ))}
-      </div>
+                    onClick={() => handleColorChange(color)}
+                >
+                    <div
+                        className="w-full h-full rounded-md"
+                        style={{ backgroundColor: color }}
+                    />
+                </button>
+            ))}
+        </div>
     );
-  };
+};
